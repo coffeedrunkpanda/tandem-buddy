4d3c035bc25c31abd539fae6702559470c207366
coffeedrunkpanda <52378774+coffeedrunkpanda@users.noreply.github.com>
2025-11-30 19:40:24 +0100

    Added functionality, example and minor refactor
    Added leadning underscore to private members
    Added save audio functionality
    Added an example of usage in the end.

--- audio_processing.py
+++ audio_processing.py
@@ -1,13 +1,15 @@
 import os
+import types
 from elevenlabs.client import ElevenLabs
 from dotenv import load_dotenv 
 
 load_dotenv()
 
+# TODO: add transcription params to the .env file
 
 class AudioProcessing():
 
-    def __init__(self):
+    def __init__(self) -> None:
     
         api_key = os.getenv("ELEVENLABS_API_KEY")
         voice_id = os.getenv("ELEVENLABS_VOICE_ID")
@@ -18,27 +20,27 @@ class AudioProcessing():
         if not voice_id:
             raise ValueError("ELEVENLABS_VOICE_ID not found")
     
-        self.client = ElevenLabs(api_key=api_key)
+        self._client = ElevenLabs(api_key=api_key)
 
-        self.transcription_params = {
+        self._transcription_params = {
             "model_id": "scribe_v1",
             "language_code": "es",
         }
 
-        self.text_to_speech_params = {
+        self._text_to_speech_params = {
             "voice_id": voice_id
         }
 
-    def transcribe_audio(self, audio_data):
+    def speech_to_text(self, audio_data)-> str:
         """Transcribe audio to text with elevenlabs"""
         
         if audio_data is None:
             return None        
         
-        transcription = self.client.speech_to_text.convert(
+        transcription = self._client.speech_to_text.convert(
             file=audio_data,
-            model_id=self.transcription_params["model_id"],
-            language_code=self.transcription_params["language_code"],
+            model_id=self._transcription_params["model_id"],
+            language_code=self._transcription_params["language_code"],
         )    
         
         return transcription.text
@@ -46,13 +48,47 @@ class AudioProcessing():
     def text_to_speech(self, text):
         """Convert text to speech with elevenlabs"""
         
-        audio = self.client.text_to_dialogue.convert(
+        audio = self._client.text_to_dialogue.convert(
             inputs=[
                 {
                     "text": text,
-                    "voice_id": self.text_to_speech_params["voice_id"],
+                    "voice_id": self._text_to_speech_params["voice_id"],
                 }
             ]
         )
 
         return audio
+
+    def save_audio_to_file(self, audio_data, filename) -> None:
+        if audio_data is None:
+            return None
+
+        if isinstance(audio_data, types.GeneratorType):  
+            with open(filename, "wb") as out_file:
+                for chunk in audio_data:
+                    out_file.write(chunk)
+
+        elif isinstance(audio_data, bytes):
+            with open(filename, "wb") as out_file:
+                out_file.write(audio_data)
+
+if __name__ == "__main__":
+    audio_processor = AudioProcessing()
+    
+    # Example usage speech to text
+    audio_file_path = "teste.m4a"
+
+    with open(audio_file_path, "rb") as f:
+        audio_data = f.read()
+        print(type(audio_data))
+        transcription_text = audio_processor.speech_to_text(audio_data)
+        print("Transcription:", transcription_text)
+
+ 
+    # Example usage text to speech
+    text = "Hola, ¿cómo estás, amiga? Espero que tengas un gran día, guapa."
+    audio_response = audio_processor.text_to_speech(text)
+    print(type(audio_response))
+
+    # Example saving audio response to file
+    audio_processor.save_audio_to_file(audio_response, "temp/audio_response_x.mp3")
